477cff7ea49d6511acf6611ea55f390ab8ba6fdf
Trevor Pao <shuaib25@gmail.com>
2017-04-19 13:35:21 +0800

    use gulp

---
+++ README.md
@@ -0,0 +1,34 @@
+geneEH
+======
+
+Small Flow Controller by Genes.
+
+
+## Documentation
+
+### Links
+
+* Home page - [https://github.com/trevorpao/operon](https://github.com/trevorpao/operon)
+* Demo page - [https://trevorpao.github.io/operon/](https://trevorpao.github.io/operon/)
+
+### Dependencies
+- [gene-event-handler](https://github.com/trevorpao/geneEH)
+
+### Installation
+
+
+## Contribute
+
+You're more than welcome to contribute to this project. 
+
+* Run `gulp serve` to preview and watch for changes
+* Run `bower install` --save <package> to install frontend dependencies
+* Run `gulp serve:test` to run the tests in the browser
+* Run `gulp` to build your webapp for production
+* Run `gulp serve:dist` to preview the production build
+
+Enjoy!
+
+## Bug tracker
+
+If you find a bug, please report it [here on Github](https://github.com/trevorpao/operon/issues)!
---
+++ app/scripts/modules/gallery.js
@@ -0,0 +1,38 @@
+;(function(app, gee, $){
+    "use strict";
+
+    // register a module name
+    app.gallery = {
+
+        baseIfrme: function() {
+            return '<div class="gene-iframe app-%(prefix)s"><iframe id="ifr_%(uniqid)s" src="'+ location.href +'%(taUri)s" width="%(width)s" scrolling="no" frameborder="0" allowtransparency="true" role="application" style="width: %(width)s; z-index: %(zidx)s;" class="app-box"></iframe></div>';
+        },
+
+        renderOpt: function(prefix, width, zidx) {
+            var opt = {
+                uniqid: prefix + Math.floor(Math.random() * 999 + 1),
+                taUri: 'tmpls/'+ prefix +'.html?pc=XXXX&uc=xxxx',
+                width: width,
+                zidx: zidx,
+                prefix: prefix
+            };
+
+            opt.taUri += '&ta=' + opt.uniqid +'&canonical=' + decodeURIComponent($('link[rel=canonical]').attr('href'));
+
+            return opt;
+        }
+    };
+
+    gee.hookTag('gee\\:gallery-grid', function(me) {
+        var opt = app.gallery.renderOpt('gallery-grid', '100%', '5');
+        var attr = app.extractAttr(me);
+
+        opt.taUri += '&page='+ attr.page;
+
+        me.replaceWith(sprintf(app.gallery.baseIfrme(), opt));
+    });
+
+    // <gee:gallery-grid page="123456"></gee:gallery-grid>
+
+
+}(app, gee, jQuery));
