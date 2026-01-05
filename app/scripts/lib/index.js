c995b8d75712580529a133f07646b456660c9ab9
Trevor Pao <shuaib25@gmail.com>
2026-01-06 06:57:14 +0800

    chore: add lib index for plugin utilities

---
+++ app/scripts/lib/index.js
@@ -0,0 +1,8 @@
+export { default as defaultPlugin, createPlugin } from './defaultPlugin';
+export { default as detect } from './detect';
+export { default as event } from './event';
+export { default as head } from './head';
+export { default as extend } from './extend';
+export { default as formatHelper } from './formatHelper';
+export { default as sprintf } from './sprintf';
+export { default as postMessage } from './jquery.ba-postmessage';
