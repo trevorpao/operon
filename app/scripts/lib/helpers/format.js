de00128ed91de8f768080d166e718da2a2671ad2
Trevor Pao <shuaib25@gmail.com>
2026-01-08 19:46:09 +0800

    fix format MD5

--- app/scripts/lib/helpers/format.js
+++ app/scripts/lib/helpers/format.js
@@ -42,7 +42,150 @@ const baseConverter = (nbasefrom, basefrom, baseto) => {
     return nbaseten.toString();
 };
 
-const MD5 = function(s){function L(k,d){return(k<<d)|(k>>>(32-d));}function K(G,k){var I,d,F,H,x;F=(G&2147483648);H=(k&2147483648);I=(G&1073741824);d=(k&1073741824);x=(G&1073741823)+(k&1073741823);if(I&d){return(x^2147483648^F^H);}if(I|d){if(x&1073741824){return(x^3221225472^F^H);}else{return(x^1073741824^F^H);}}else{return(x^F^H);}function r(d,F,k){return(d&F)|((~d)&k);}function q(d,F,k){return(d&k)|(F&(~k));}function p(d,F,k){return(d^F^k);}function n(d,F,k){return(F^(d|(~k)));}function u(G,F,aa,Z,k,H,I){G=K(G,K(K(r(F,aa,Z),k),I));return K(L(G,H),F);}function f(G,F,aa,Z,k,H,I){G=K(G,K(K(q(F,aa,Z),k),I));return K(L(G,H),F);}function D(G,F,aa,Z,k,H,I){G=K(G,K(K(p(F,aa,Z),k),I));return K(L(G,H),F);}function t(G,F,aa,Z,k,H,I){G=K(G,K(K(n(F,aa,Z),k),I));return K(L(G,H),F);}function e(G){var Z;var F=G.length;var x=F+8;var k=(x-(x%64))/64;var I=(k+1)*16;var aa=Array(I-1);var d=0;var H=0;while(H<F){Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=(aa[Z]|(G.charCodeAt(H)<<d));H++;}Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=aa[Z]|(128<<d);aa[I-2]=F<<3;aa[I-1]=F>>>29;return aa;}function B(x){var k='',F='',G,d;for(d=0;d<=3;d++){G=(x>>>(d*8))&255;F='0'+G.toString(16);k=k+F.substr(F.length-2,2);}return k;}function J(k){k=k.replace(/rn/g,'n');var d='';for(var F=0;F<k.length;F++){var x=k.charCodeAt(F);if(x<128){d+=String.fromCharCode(x);}else{if((x>127)&&(x<2048)){d+=String.fromCharCode((x>>6)|192);d+=String.fromCharCode((x&63)|128);}else{d+=String.fromCharCode((x>>12)|224);d+=String.fromCharCode(((x>>6)&63)|128);d+=String.fromCharCode((x&63)|128);}}}return d;}var C=Array();var P,h,E,v,g,Y,X,W,V;var S=7,Q=12,N=17,M=22;var A=5,z=9,y=14,w=20;var o=4,m=11,l=16,j=23;var U=6,T=10,R=15,O=21;s=J(s);C=e(s);Y=1732584193;X=4023233417;W=2562383102;V=271733878;for(P=0;P<C.length;P+=16){h=Y;E=X;v=W;g=V;Y=u(Y,X,W,V,C[P+0],S,3614090360);V=u(V,Y,X,W,C[P+1],Q,3905402710);W=u(W,V,Y,X,C[P+2],N,606105819);X=u(X,W,V,Y,C[P+3],M,3250441966);Y=u(Y,X,W,V,C[P+4],S,4118548399);V=u(V,Y,X,W,C[P+5],Q,1200080426);W=u(W,V,Y,X,C[P+6],N,2821735955);X=u(X,W,V,Y,C[P+7],M,4249261313);Y=u(Y,X,W,V,C[P+8],S,1770035416);V=u(V,Y,X,W,C[P+9],Q,2336552879);W=u(W,V,Y,X,C[P+10],N,4294925233);X=u(X,W,V,Y,C[P+11],M,2304563134);Y=u(Y,X,W,V,C[P+12],S,1804603682);V=u(V,Y,X,W,C[P+13],Q,4254626195);W=u(W,V,Y,X,C[P+14],N,2792965006);X=u(X,W,V,Y,C[P+15],M,1236535329);Y=f(Y,X,W,V,C[P+1],A,4129170786);V=f(V,Y,X,W,C[P+6],z,3225465664);W=f(W,V,Y,X,C[P+11],y,643717713);X=f(X,W,V,Y,C[P+0],w,3921069994);Y=f(Y,X,W,V,C[P+5],A,3593408605);V=f(V,Y,X,W,C[P+10],z,38016083);W=f(W,V,Y,X,C[P+15],y,3634488961);X=f(X,W,V,Y,C[P+4],w,3889429448);Y=f(Y,X,W,V,C[P+9],A,568446438);V=f(V,Y,X,W,C[P+14],z,3275163606);W=f(W,V,Y,X,C[P+3],y,4107603335);X=f(X,W,V,Y,C[P+8],w,1163531501);Y=f(Y,X,W,V,C[P+13],A,2850285829);V=f(V,Y,X,W,C[P+2],z,4243563512);W=f(W,V,Y,X,C[P+7],y,1735328473);X=f(X,W,V,Y,C[P+12],w,2368359562);Y=D(Y,X,W,V,C[P+5],o,4294588738);V=D(V,Y,X,W,C[P+8],m,2272392833);W=D(W,V,Y,X,C[P+11],l,1839030562);X=D(X,W,V,Y,C[P+14],j,4259657740);Y=D(Y,X,W,V,C[P+1],o,2763975236);V=D(V,Y,X,W,C[P+4],m,1272893353);W=D(W,V,Y,X,C[P+7],l,4139469664);X=D(X,W,V,Y,C[P+10],j,3200236656);Y=D(Y,X,W,V,C[P+13],o,681279174);V=D(V,Y,X,W,C[P+0],m,3936430074);W=D(W,V,Y,X,C[P+3],l,3572445317);X=D(X,W,V,Y,C[P+6],j,76029189);Y=D(Y,X,W,V,C[P+9],o,3654602809);V=D(V,Y,X,W,C[P+12],m,3873151461);W=D(W,V,Y,X,C[P+15],l,530742520);X=D(X,W,V,Y,C[P+2],j,3299628645);Y=t(Y,X,W,V,C[P+0],U,4096336452);V=t(V,Y,X,W,C[P+7],T,1126891415);W=t(W,V,Y,X,C[P+14],R,2878612391);X=t(X,W,V,Y,C[P+5],O,4237533241);Y=t(Y,X,W,V,C[P+12],U,1700485571);V=t(V,Y,X,W,C[P+3],T,2399980690);W=t(W,V,Y,X,C[P+10],R,4293915773);X=t(X,W,V,Y,C[P+1],O,2240044497);Y=t(Y,X,W,V,C[P+8],U,1873313359);V=t(V,Y,X,W,C[P+15],T,4264355552);W=t(W,V,Y,X,C[P+6],R,2734768916);X=t(X,W,V,Y,C[P+13],O,1309151649);Y=t(Y,X,W,V,C[P+4],U,4149444226);V=t(V,Y,X,W,C[P+11],T,3174756917);W=t(W,V,Y,X,C[P+2],R,718787259);X=t(X,W,V,Y,C[P+9],O,3951481745);Y=K(Y,h);X=K(X,E);W=K(W,v);V=K(V,g);}var i=B(Y)+B(X)+B(W)+B(V);return i.toLowerCase();};
+// Lightweight MD5 (hex, lowercase) for gravatar/legacy helpers; replaces broken minified copy
+// source adapted from blueimp/JavaScript-MD5 (MIT) but trimmed for our use-case
+const MD5 = (str = '') => {
+    const rotateLeft = (lValue, iShiftBits) => ((lValue << iShiftBits) | (lValue >>> (32 - iShiftBits))) >>> 0;
+    const addUnsigned = (lX, lY) => {
+        const lX4 = lX & 0x40000000;
+        const lY4 = lY & 0x40000000;
+        const lX8 = lX & 0x80000000;
+        const lY8 = lY & 0x80000000;
+        const lResult = (lX & 0x3fffffff) + (lY & 0x3fffffff);
+        if (lX4 & lY4) return (lResult ^ 0x80000000 ^ lX8 ^ lY8) >>> 0;
+        if (lX4 | lY4) {
+            return (lResult & 0x40000000)
+                ? (lResult ^ 0xc0000000 ^ lX8 ^ lY8) >>> 0
+                : (lResult ^ 0x40000000 ^ lX8 ^ lY8) >>> 0;
+        }
+        return (lResult ^ lX8 ^ lY8) >>> 0;
+    };
+
+    const F = (x, y, z) => ((x & y) | (~x & z)) >>> 0;
+    const G = (x, y, z) => ((x & z) | (y & ~z)) >>> 0;
+    const H = (x, y, z) => (x ^ y ^ z) >>> 0;
+    const I = (x, y, z) => (y ^ (x | ~z)) >>> 0;
+
+    const FF = (a, b, c, d, x, s, ac) => addUnsigned(rotateLeft(addUnsigned(addUnsigned(a, F(b, c, d)), addUnsigned(x, ac)), s), b);
+    const GG = (a, b, c, d, x, s, ac) => addUnsigned(rotateLeft(addUnsigned(addUnsigned(a, G(b, c, d)), addUnsigned(x, ac)), s), b);
+    const HH = (a, b, c, d, x, s, ac) => addUnsigned(rotateLeft(addUnsigned(addUnsigned(a, H(b, c, d)), addUnsigned(x, ac)), s), b);
+    const II = (a, b, c, d, x, s, ac) => addUnsigned(rotateLeft(addUnsigned(addUnsigned(a, I(b, c, d)), addUnsigned(x, ac)), s), b);
+
+    const toWordArray = (msg) => {
+        const lMessageLength = msg.length;
+        const lNumberOfWordsTemp1 = lMessageLength + 8;
+        const lNumberOfWordsTemp2 = ((lNumberOfWordsTemp1 - (lNumberOfWordsTemp1 % 64)) / 64 + 1) * 16;
+        const lWordArray = new Array(lNumberOfWordsTemp2).fill(0);
+        for (let i = 0; i < lMessageLength; i += 1) {
+            const wordCount = (i - (i % 4)) / 4;
+            const bytePosition = (i % 4) * 8;
+            lWordArray[wordCount] |= msg.charCodeAt(i) << bytePosition;
+        }
+        const wordCount = (lMessageLength - (lMessageLength % 4)) / 4;
+        const bytePosition = (lMessageLength % 4) * 8;
+        lWordArray[wordCount] |= 0x80 << bytePosition;
+        lWordArray[lNumberOfWordsTemp2 - 2] = lMessageLength << 3;
+        lWordArray[lNumberOfWordsTemp2 - 1] = lMessageLength >>> 29;
+        return lWordArray;
+    };
+
+    const wordToHex = (lValue) => {
+        let wordToHexValue = '';
+        for (let lCount = 0; lCount <= 3; lCount += 1) {
+            const lByte = (lValue >>> (lCount * 8)) & 255;
+            const temp = `0${lByte.toString(16)}`;
+            wordToHexValue += temp.slice(temp.length - 2);
+        }
+        return wordToHexValue;
+    };
+
+    const utf8Encode = (msg) => unescape(encodeURIComponent(msg));
+
+    const x = toWordArray(utf8Encode(String(str)));
+    let a = 0x67452301;
+    let b = 0xefcdab89;
+    let c = 0x98badcfe;
+    let d = 0x10325476;
+
+    for (let k = 0; k < x.length; k += 16) {
+        const [aa, bb, cc, dd] = [a, b, c, d];
+
+        a = FF(a, b, c, d, x[k + 0], 7, 0xd76aa478);
+        d = FF(d, a, b, c, x[k + 1], 12, 0xe8c7b756);
+        c = FF(c, d, a, b, x[k + 2], 17, 0x242070db);
+        b = FF(b, c, d, a, x[k + 3], 22, 0xc1bdceee);
+        a = FF(a, b, c, d, x[k + 4], 7, 0xf57c0faf);
+        d = FF(d, a, b, c, x[k + 5], 12, 0x4787c62a);
+        c = FF(c, d, a, b, x[k + 6], 17, 0xa8304613);
+        b = FF(b, c, d, a, x[k + 7], 22, 0xfd469501);
+        a = FF(a, b, c, d, x[k + 8], 7, 0x698098d8);
+        d = FF(d, a, b, c, x[k + 9], 12, 0x8b44f7af);
+        c = FF(c, d, a, b, x[k + 10], 17, 0xffff5bb1);
+        b = FF(b, c, d, a, x[k + 11], 22, 0x895cd7be);
+        a = FF(a, b, c, d, x[k + 12], 7, 0x6b901122);
+        d = FF(d, a, b, c, x[k + 13], 12, 0xfd987193);
+        c = FF(c, d, a, b, x[k + 14], 17, 0xa679438e);
+        b = FF(b, c, d, a, x[k + 15], 22, 0x49b40821);
+
+        a = GG(a, b, c, d, x[k + 1], 5, 0xf61e2562);
+        d = GG(d, a, b, c, x[k + 6], 9, 0xc040b340);
+        c = GG(c, d, a, b, x[k + 11], 14, 0x265e5a51);
+        b = GG(b, c, d, a, x[k + 0], 20, 0xe9b6c7aa);
+        a = GG(a, b, c, d, x[k + 5], 5, 0xd62f105d);
+        d = GG(d, a, b, c, x[k + 10], 9, 0x02441453);
+        c = GG(c, d, a, b, x[k + 15], 14, 0xd8a1e681);
+        b = GG(b, c, d, a, x[k + 4], 20, 0xe7d3fbc8);
+        a = GG(a, b, c, d, x[k + 9], 5, 0x21e1cde6);
+        d = GG(d, a, b, c, x[k + 14], 9, 0xc33707d6);
+        c = GG(c, d, a, b, x[k + 3], 14, 0xf4d50d87);
+        b = GG(b, c, d, a, x[k + 8], 20, 0x455a14ed);
+        a = GG(a, b, c, d, x[k + 13], 5, 0xa9e3e905);
+        d = GG(d, a, b, c, x[k + 2], 9, 0xfcefa3f8);
+        c = GG(c, d, a, b, x[k + 7], 14, 0x676f02d9);
+        b = GG(b, c, d, a, x[k + 12], 20, 0x8d2a4c8a);
+
+        a = HH(a, b, c, d, x[k + 5], 4, 0xfffa3942);
+        d = HH(d, a, b, c, x[k + 8], 11, 0x8771f681);
+        c = HH(c, d, a, b, x[k + 11], 16, 0x6d9d6122);
+        b = HH(b, c, d, a, x[k + 14], 23, 0xfde5380c);
+        a = HH(a, b, c, d, x[k + 1], 4, 0xa4beea44);
+        d = HH(d, a, b, c, x[k + 4], 11, 0x4bdecfa9);
+        c = HH(c, d, a, b, x[k + 7], 16, 0xf6bb4b60);
+        b = HH(b, c, d, a, x[k + 10], 23, 0xbebfbc70);
+        a = HH(a, b, c, d, x[k + 13], 4, 0x289b7ec6);
+        d = HH(d, a, b, c, x[k + 0], 11, 0xeaa127fa);
+        c = HH(c, d, a, b, x[k + 3], 16, 0xd4ef3085);
+        b = HH(b, c, d, a, x[k + 6], 23, 0x04881d05);
+        a = HH(a, b, c, d, x[k + 9], 4, 0xd9d4d039);
+        d = HH(d, a, b, c, x[k + 12], 11, 0xe6db99e5);
+        c = HH(c, d, a, b, x[k + 15], 16, 0x1fa27cf8);
+        b = HH(b, c, d, a, x[k + 2], 23, 0xc4ac5665);
+
+        a = II(a, b, c, d, x[k + 0], 6, 0xf4292244);
+        d = II(d, a, b, c, x[k + 7], 10, 0x432aff97);
+        c = II(c, d, a, b, x[k + 14], 15, 0xab9423a7);
+        b = II(b, c, d, a, x[k + 5], 21, 0xfc93a039);
+        a = II(a, b, c, d, x[k + 12], 6, 0x655b59c3);
+        d = II(d, a, b, c, x[k + 3], 10, 0x8f0ccc92);
+        c = II(c, d, a, b, x[k + 10], 15, 0xffeff47d);
+        b = II(b, c, d, a, x[k + 1], 21, 0x85845dd1);
+        a = II(a, b, c, d, x[k + 8], 6, 0x6fa87e4f);
+        d = II(d, a, b, c, x[k + 15], 10, 0xfe2ce6e0);
+        c = II(c, d, a, b, x[k + 6], 15, 0xa3014314);
+        b = II(b, c, d, a, x[k + 13], 21, 0x4e0811a1);
+        a = II(a, b, c, d, x[k + 4], 6, 0xf7537e82);
+        d = II(d, a, b, c, x[k + 11], 10, 0xbd3af235);
+        c = II(c, d, a, b, x[k + 2], 15, 0x2ad7d2bb);
+        b = II(b, c, d, a, x[k + 9], 21, 0xeb86d391);
+
+        a = addUnsigned(a, aa);
+        b = addUnsigned(b, bb);
+        c = addUnsigned(c, cc);
+        d = addUnsigned(d, dd);
+    }
+
+    return (wordToHex(a) + wordToHex(b) + wordToHex(c) + wordToHex(d)).toLowerCase();
+};
 
 const bytesLength = (str) => {
     var n = (str || '').length;
--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,65 @@
+const path = require('path');
 const { defineConfig } = require('vite');
 const viteTwig = require('vite-twig-ssr');
 
-const redirectRootPlugin = () => ({
-  name: 'redirect-root-to-twig',
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.url === '/' || req.url === '/index.html') {
-        res.statusCode = 302;
-        res.setHeader('Location', '/app/themes/default/ssr/index.twig');
-        return res.end();
-      }
-      return next();
-    });
-  },
+const VIEWS_PREFIX = '/app/themes/default/ssr/';
+
+const devRoutingPlugin = () => ({
+    name: 'docute-dev-routing',
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            if (!req.url) {
+                return next();
+            }
+
+            if (req.url === '/' || req.url === '/index.html') {
+                res.statusCode = 302;
+                res.setHeader('Location', `${VIEWS_PREFIX}index.twig`);
+                return res.end();
+            }
+
+            if (req.url.startsWith(VIEWS_PREFIX) && req.url.endsWith('.twig')) {
+                req.url = `/${req.url.slice(VIEWS_PREFIX.length)}`;
+            }
+
+            return next();
+        });
+    },
 });
 
 module.exports = defineConfig({
-  root: __dirname,
-  plugins: [
-    redirectRootPlugin(),
-    viteTwig({
-      viewsPath: './app/themes/default/ssr/',
-      mockPath: './mock',
-      globalData: {
-        feVersion: '0.0.1-dev',
-        assetsUri: '/assets/',
-        opts: {
-          default: {
-            contact_mail: 'dev-team@example.com',
-          },
+    root: __dirname,
+    resolve: {
+        alias: {
+            'trevorpao/geneEH': path.resolve(__dirname, 'app/scripts/lib/gee-bridge.js'),
         },
-      },
-      filters: {
-        headline: (value) => String(value || '').toUpperCase(),
-      },
-      functions: {
-        asset: (value) => `/assets/${value}`,
-      },
-    }),
-  ],
-  server: {
-    open: '/app/themes/default/ssr/index.twig',
-    watch: {
-      ignored: ['**/node_modules/**', '**/dist/**'],
     },
-  },
+    plugins: [
+        devRoutingPlugin(),
+        viteTwig({
+            viewsPath: './app/themes/default/ssr/',
+            mockPath: './app/mock',
+            globalData: {
+                feVersion: '0.0.1-dev',
+                assetsUri: '/assets/',
+                opts: {
+                    default: {
+                        contact_mail: 'dev-team@example.com',
+                    },
+                },
+            },
+            filters: {
+                headline: (value) => String(value || '').toUpperCase(),
+            },
+            functions: {
+                asset: (value) => `/assets/${value}`,
+            },
+        }),
+    ],
+    server: {
+        open: '/app/themes/default/ssr/index.twig',
+        watch: {
+            ignored: ['**/node_modules/**', '**/dist/**'],
+        },
+    },
 });
